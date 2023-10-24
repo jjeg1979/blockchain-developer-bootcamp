50bd6569492210e67b6ebae7f8126b5a97fe9f85
jjeg1979 <jjeg1979@gmail.com>
2023-10-24 06:48:52 +0200

    2. Smoke Test

---
+++ contracts/Token.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// To be able to do console logging while deploying contracts
+import "hardhat/console.sol";
+
+contract Token{
+    string public name = "My Token";
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// You  need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/** 
+ * @type import('hardhat/config').HardhatUserConfig 
+*/
 module.exports = {
   solidity: "0.8.18",
+  // This line connects the project to the blockchain
+  networks: {
+    localhost: {},
+  },
 };
